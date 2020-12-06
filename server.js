e333a26326d5470f0f96ad12747f54930f8305ad
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-12-06 12:23:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,44 @@ const { response } = require("express")
 const cors = require("cors")
 const app = express()
 const port = process.env.PORT||3000
-const quizzesdata = require("./data")
-const scores = []
+const {services} = require("./data")
+
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+  
+app.get('/search/:searchTerm/:location',(request, response) => {
+    const term = request.params.searchTerm
+    const [city,state] = request.params.location.split(",")
+    console.log("TCS",term,city,state)
+    services.findbusinesses(response,term,city,state)
+})
+app.post('/place', (request, response) => {
+    console.log("place:", request.body)
+    if (request.body.update){
+        services.updatebusiness(response,request.body)
 
-app.get('/quizzes', (request, response) => {
-    response.json(quizzesdata.quizzes)
+    }
+    else {
+        services.addbusiness(response,request.body)  
+    }
+    
+
 })
 
-app.get('/quiz/:id',(request, response) => {
-    let found = quizzesdata.questions["Quiz"+request.params.id]
-    response.json(found)
+app.get('/places',(request, response) => {
+    console.log("services",services)
+    console.log("allb", services.allbusinesses)
+    services.allbusinesses(response)
 })
-app.post('/score', (request, response) => {
-    scores.push(request.body)
-    response.json({message: 'Score has saved'})
-
-}
-)
+app.post('/review/:placeId',(request, response) => { 
+    let placeid = parseInt(request.params.placeId)
+    console.log(request.body)
+    let review = request.body.review
+    services.addreview(placeid,review)
+})
 
 app.listen(port, () => {
-    console.log(`Port: ${port}`)
+    console.log(`Example app listening on port ${port}!`)
 })
