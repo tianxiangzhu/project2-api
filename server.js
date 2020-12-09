4dd886f7fb11069958b4f7935e5d8b407f67565a
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-12-08 22:32:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ app.use(express.urlencoded({extended: true}))
 app.get('/search/:searchTerm/:location',(request, response) => {
     const term = request.params.searchTerm
     const [city,state] = request.params.location.split(",")
-    console.log("TCS",term,city,state)
     services.findbusinesses(response,term,city,state)
 })
 app.post('/place', (request, response) => {
-    console.log("place:", request.body)
     if (request.body.update){
         services.updatebusiness(response,request.body)
-
     }
     else {
         services.addbusiness(response,request.body)  
@@ -30,17 +27,19 @@ app.post('/place', (request, response) => {
 })
 
 app.get('/places',(request, response) => {
-    console.log("services",services)
-    console.log("allb", services.allbusinesses)
     services.allbusinesses(response)
 })
 app.post('/review/:placeId',(request, response) => { 
-    let placeid = parseInt(request.params.placeId)
-    console.log(request.body)
+    let place = parseInt(request.params.placeId)
     let review = request.body.review
-    services.addreview(placeid,review)
+    services.addreview(place,review)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log('${port}')
+})
+
+app.delete('/place/:placeId',(request, response) => {
+    let place = parseInt(request.params.placeId)
+    services.deletebusiness(response,place)
 })
